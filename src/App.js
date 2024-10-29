9e51b80b0d10f246587f0f2c9418cd16f8ca0795
mateuspadilha166 <mateuspadilha873@gmail.com>
2024-10-29 19:53:34 -0300

    cmt

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import RoutesApp from "./router";
 
 function App(){
--- src/pages/Sobre/index.js
+++ src/pages/Sobre/index.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Link } from "react-router-dom";
 import './sobre.css';
 import gogeta from './gogeta.jpg'; 
