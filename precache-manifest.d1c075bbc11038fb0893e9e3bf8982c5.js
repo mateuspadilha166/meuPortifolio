9a9aeae40b5c6c584ade9355ec5b8788f036e87e
mateuspadilha166 <mateuspadilha873@gmail.com>
2024-10-29 19:54:11 -0300

    Updates

---
+++ precache-manifest.d1c075bbc11038fb0893e9e3bf8982c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19f74b0fb2ae0def9586b4819a11c08c",
+    "url": "/meuPortifolio/index.html"
+  },
+  {
+    "revision": "8cbf81bf11423ef69b7d",
+    "url": "/meuPortifolio/static/css/2.b71989ea.chunk.css"
+  },
+  {
+    "revision": "ba26985b1e195a8ef50f",
+    "url": "/meuPortifolio/static/css/main.137d636a.chunk.css"
+  },
+  {
+    "revision": "8cbf81bf11423ef69b7d",
+    "url": "/meuPortifolio/static/js/2.1a53fdc9.chunk.js"
+  },
+  {
+    "revision": "ba26985b1e195a8ef50f",
+    "url": "/meuPortifolio/static/js/main.aa5a71f3.chunk.js"
+  },
+  {
+    "revision": "cdc1ce34fa6dfa3133c0",
+    "url": "/meuPortifolio/static/js/runtime~main.a5b5c045.js"
+  },
+  {
+    "revision": "808a9f3492b5be5df817b960e17c5c66",
+    "url": "/meuPortifolio/static/media/gogeta.808a9f34.jpg"
+  }
+]);
